134a9c8ca56117faaed26090ffa225db7d439413
YazanAboKasem <yazan.abo.kasem@gmail.com>
2026-03-30 10:21:13 +0400

    add vendorf

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-//dfgfd fdfgfg gg
+//dfgfd fdfgfg ggggg
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
