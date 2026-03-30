f3282a91022a01d56f95940f6be2738ac5ae4363
YazanAboKasem <yazan.abo.kasem@gmail.com>
2026-03-30 09:49:20 +0400

    first235t

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-//dfgfd fdfgfg
+//dfgfd fdfgfg gg
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
